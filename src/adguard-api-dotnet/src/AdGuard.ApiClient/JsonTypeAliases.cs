160c124951f85b33d53fe0ff9c2493da4cdb1643
Jayson Knight <jayson.knight@jaysonknight.com>
2026-07-28 22:48:57 -0400

    chore(adguard): extend JSON alias file to cover common ambiguous names and trigger CI

--- src/adguard-api-dotnet/src/AdGuard.ApiClient/JsonTypeAliases.cs
+++ src/adguard-api-dotnet/src/AdGuard.ApiClient/JsonTypeAliases.cs
@@ -1,8 +1,17 @@
 // Prevent ambiguity between Newtonsoft.Json and System.Text.Json types used in generated code.
-// Map unqualified names used by generated classes to Newtonsoft.Json equivalents.
+// Map short/unqualified JSON type/attribute names used in generated sources to Newtonsoft.Json equivalents.
 
 using JsonConstructorAttribute = Newtonsoft.Json.JsonConstructorAttribute;
 using JsonConstructor = Newtonsoft.Json.JsonConstructorAttribute;
+
+// Attributes
 using JsonConverterAttribute = Newtonsoft.Json.JsonConverterAttribute;
-using JsonConverter = Newtonsoft.Json.JsonConverter; // map to the Newtonsoft.Json.JsonConverter class
+using JsonConverter = Newtonsoft.Json.JsonConverter; // Newtonsoft.Json.JsonConverter
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
+using JsonIgnore = Newtonsoft.Json.JsonIgnoreAttribute;
+using JsonPropertyAttribute = Newtonsoft.Json.JsonPropertyAttribute;
+using JsonProperty = Newtonsoft.Json.JsonPropertyAttribute;
+
+// Serializer types
 using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+using JsonSerializerSettings = Newtonsoft.Json.JsonSerializerSettings;
